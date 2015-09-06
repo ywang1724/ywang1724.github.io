49d452f8b3b5e13ec7555e36d35e2af0c333f77f
ywang1724 <ywang1724@gmail.com>
2015-09-07 01:48:28 +0800

    modify for needs change

--- you++/js/index.js
+++ you++/js/index.js
@@ -1,27 +1,11 @@
 $(document).ready(function() {
-    var debouncer = function(func, timeout) {
-        var timeoutID, timeout = timeout || 200;
-        return function() {
-            var scope = this,
-                args = arguments;
-            clearTimeout(timeoutID);
-            timeoutID = setTimeout(function() {
-                func.apply(scope, Array.prototype.slice.call(args));
-            }, timeout);
-        };
-    };
+    $('#fullpage').fullpage({
+        anchors: ['firstPage', 'myIntroduce', 'myFunction', '4thpage', '5thpage', 'myTeam', 'myDownload', 'myLink'],
+        menu: '#menu',
+        scrollingSpeed: 1000
+    });
 
     $('.my-iframe').height($(window).height() - 85);
-
-    $('#myCarousel').carousel({
-        interval: false
-    }).bind('mousewheel', debouncer(function(event) {
-        if (event.originalEvent.wheelDelta >= 0) {
-            $('#myCarousel').carousel('prev');
-        } else {
-            $('#myCarousel').carousel('next');
-        }
-    }));
 
     $('#myJoin').mouseover(function() {
         $('#myJoin').attr('src', 'img/joinusselect.png');
@@ -37,20 +21,5 @@ $(document).ready(function() {
         $('#myAndroid').attr('src', 'img/android-download-select.png');
     }).mouseout(function() {
         $('#myAndroid').attr('src', 'img/android-download.png');
-    });
-
-    var clearActive = function() {
-    	$('#myCarousel .item, #myCarousel li').removeClass('active');
-    };
-
-    var attrActive = function(index) {
-    	$('#myCarousel').find('.item').eq(index).addClass('active');
-    	$('#myCarousel').find('li').eq(index).addClass('active');
-    };
-
-    $('#navbar a').on('click', function() {
-    	clearActive();
-    	attrActive($(this).attr('id'));
-    	// $(this).parent().css('background-color', '#337ab7');
     });
 });
