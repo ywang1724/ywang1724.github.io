48ea4776944394b1d24a33bf0117e6bc2d27121f
ywang1724 <ywang1724@gmail.com>
2016-03-11 00:15:08 +0800

    add ejs

--- gmxx/gulpfile.js
+++ gmxx/gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var less = require('gulp-less');
+var ejs = require("gulp-ejs");
 
-gulp.task('img', function () {
+gulp.task('img', function() {
     return gulp.src('./img/*')
         .pipe(imagemin({
             progressive: true
@@ -10,13 +11,21 @@ gulp.task('img', function () {
         .pipe(gulp.dest('./dist/img/'));
 });
 
-gulp.task('less', function () {
+gulp.task('less', function() {
     gulp.src('./less/*.less')
         .pipe(less())
-        .on('error', function(e){console.log(e)})
+        .on('error', function(e) {
+            console.log(e)
+        })
         .pipe(gulp.dest('./dist/css/'));
 });
 
-// gulp.task('default', ['img', 'less']);
+gulp.task('ejs', function() {
+    gulp.src("./ejs/*.ejs")
+        .pipe(ejs({
+            msg: "Hello Gulp!"
+        }))
+        .pipe(gulp.dest("./dist/html/"));
+});
 
-gulp.task('default', ['less']);
+gulp.task('default', ['less', 'ejs']);
